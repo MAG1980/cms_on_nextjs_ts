1a066656f70e49845ab997590790f55078fc7a51
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-12-26 21:59:17 +0300

    Конфиденциальную данные CMS Contentful переместил в переменные среды (файл .env.local).

--- contentful/index.ts
+++ contentful/index.ts
@@ -1,6 +1,9 @@
 import { createClient } from 'contentful';
 
 export const client = createClient({
-  space: 'xvkps7zt0tha',
-  accessToken: '2nu9uLfYuYsOapgKNqcz9frYUKQ84b3yTtXi94qBNfY',
+  // space: 'xvkps7zt0tha',
+  // accessToken: '2nu9uLfYuYsOapgKNqcz9frYUKQ84b3yTtXi94qBNfY',
+  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+  accessToken:
+    process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_API_ACCESS_TOKEN,
 });
