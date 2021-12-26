93ac5aecc90bacd8866b95997fae88e4db6a6249
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-12-26 22:12:55 +0300

    Обошёл ошибку с присваиванием  типа undefined  в файле с настройками доступа к Contentful.

--- contentful/index.ts
+++ contentful/index.ts
@@ -3,7 +3,7 @@ import { createClient } from 'contentful';
 export const client = createClient({
   // space: 'xvkps7zt0tha',
   // accessToken: '2nu9uLfYuYsOapgKNqcz9frYUKQ84b3yTtXi94qBNfY',
-  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID || '',
   accessToken:
-    process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_API_ACCESS_TOKEN,
+    process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_API_ACCESS_TOKEN || '',
 });
